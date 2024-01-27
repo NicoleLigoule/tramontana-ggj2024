71f2619a9089b4e2e2ddb2071d4fc1f87c03a22d
mfahler <fahler114@gmail.com>
2024-01-27 17:14:12 -0300

    Director prototipe

---
+++ Director.gd
@@ -0,0 +1,43 @@
+extends Node
+
+@export var playerIsSelectingCards = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#select la frase
+	
+	#mostrar la frase inncompleta y la cara del rey
+	
+	#mostrar las cartas-opciones para completar
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func checkCards():
+	
+	
+	pass
+	
+func checkScore():
+	
+	
+	pass
+	
+func modifyKingState():
+	
+	
+	pass
+	
+func play():
+	
+	while playerIsSelectingCards:
+		checkCards()
+		if false:
+			checkScore()
+			modifyKingState()
+			pass
+	
+	
+	pass
