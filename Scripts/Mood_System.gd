edf9ff7bebd3fb06a0fe52af899395cb42ccd469
mfahler <fahler114@gmail.com>
2024-01-28 02:02:45 -0300

    text apearing animation

--- Scripts/Mood_System.gd
+++ Scripts/Mood_System.gd
@@ -1,11 +1,17 @@
 extends Node
 
-@export var my_number = 5
+
+var node
+var ratio
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	node = get_node("Spech_sistem/RichTextLabel")
+	node.visible_ratio = 0
+	ratio= self.get_meta("ratio_visible_text")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta):	
+	node.visible_ratio += ratio*delta
 	pass
